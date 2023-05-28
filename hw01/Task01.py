bc76eb83eecb02025ea750b9fb6c777bcd3b03d4
Pavel Volchenko <2793553@gmail.com>
2023-05-28 20:20:37 +0300

    Сортировка кучей

--- hw01/Task01.py
+++ hw01/Task01.py
@@ -1,55 +1,33 @@
 def heapSort(arr):
     buildMaxHeap(arr)
-    size = len(arr) - 1
-    for i in range(size, -1, -1):
-        # print(arr)
-        arr[size], arr[0] = arr[0], arr[size]
-        size -= 1
-        siftDown(arr, size)
-        # print(arr)
+    children = len(arr)
+    for i in range(children, 1, -1):
+        # Свапаем первый элемент (максимальный по значению) с последним и забываем про него
+        # уменьшая размер массива (кол-во потомков для проверки, т.к. он уже отсортирован)
+        arr[children - 1], arr[0] = arr[0], arr[children - 1]
+        children -= 1
+        # Опять проверяем и чиним дерево, т.к. на первом месте может стоять элемент
+        # с любым значением и ломать наше дерево
+        siftDown(arr, children)
     return arr
 
+# Преобразования исходного массива в бинарное дерево.
 def buildMaxHeap(arr):
-    size = len(arr)
-    for i in range(size // 2, -1, -1):
-        # print(arr)
-        siftDown(arr, size)
-        # print(arr)
+    [siftDown(arr, len(arr)) for _ in range(len(arr) // 2, 1, -1)]
+    return arr
+
+# "Чиним" бинарное дерево (функция "утапливания" узлов).
+# Если значение потомка больше родительского, они меняются местами.
+# Проверяем все узлы, снизу в верх (к корневому узлу).
+def siftDown(arr, children):
+    for child in range(children, 1, -1):
+        parent = child // 2 - 1
+        if arr[child - 1] > arr[parent]:
+            arr[child - 1], arr[parent] = arr[parent], arr[child - 1]
     return arr
 
 
-def siftDown(arr, i):
-    for k in range(i//2-1, -1, -1):
-        # if k == 0:
-        #     left = 1
-        #     right = 2
-        # else:
-        left = 2 * k + 1
-        right = 2 * k + 2
-        print(k, left, right)
-        if arr[left] > arr[right]:
-            if arr[k] < arr[left]:
-                arr[k], arr[left] = arr[left], arr[k]
-                print(arr)
-            # else:
-            #     break
-        else:
-            if arr[k] < arr[right]:
-                arr[k], arr[right] = arr[right], arr[k]
-                print(arr)
-            # else:
-            #     break
-
-        # print(arr)
-    return arr
-
-
-arr = [4, 3, 2, 5, 1, 7, 10, 12, 14, 22, 6]
-buildMaxHeap(arr)
-
-# res = heapSort(arr)
-# print(res)
-# res2 = heapSort(res)
-# print(res2)
-
-# print(heapSort(arr))
+arr = [4, 3, 2, 5, 1, 4, 5, 5, 7, 10, 12, 12, 14, 22, 6, 9]
+print("Исходный массив:", arr)
+print("Бинарное дерево:", buildMaxHeap(arr))
+print("Отсортированный:", heapSort(arr))
