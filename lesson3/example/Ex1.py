fc00e40256e1b5ae0ddfdebf63f6abb565c7c993
Pavel Volchenko <2793553@gmail.com>
2023-05-31 14:06:58 +0300

    homework 2 is done (Reverse LinkedList)

---
+++ lesson3/example/Ex1.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+
+arr = LinkedList()
+[arr.append(i) for i in range(1, 10)]
+arr.print_list()
+
--- lesson3/homework/Task01.py
+++ lesson3/homework/Task01.py
@@ -21,5 +21,24 @@ class LinkedList:
     def print_list(self):
         current = self.head
         while current:
-            print(current.data)
+            print(current.data, end=' ')
             current = current.next
+        print()
+
+    def reverse_list(self):
+        tail = None
+        current = self.head
+        while current:
+            temp = current.next
+            current.next = tail
+            tail = current
+            current = temp
+        self.head = tail
+
+
+arr = LinkedList()
+[arr.append(i) for i in range(1, 10)]
+
+arr.print_list()
+arr.reverse_list()
+arr.print_list()
